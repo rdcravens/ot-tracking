5268005ae1a3fa8c6a493365f12f20a8f242dee8
rdcravens <rodger.cravens@gmail.com>
2020-08-09 11:06:40 -0500

    Update files.php

--- files.php
+++ files.php
@@ -1,31 +1,9 @@
 <?php
 
-function dir_tree($dir_path)
-{
-    $rdi = new \RecursiveDirectoryIterator($dir_path);
-
-    $rii = new \RecursiveIteratorIterator($rdi);
-
-    $tree = [];
-
-    foreach ($rii as $splFileInfo) {
-        $file_name = $splFileInfo->getFilename();
-
-        // Skip hidden files and directories.
-        if ($file_name[0] === '.') {
-            continue;
-        }
-
-        $path = $splFileInfo->isDir() ? array($file_name => array()) : array($file_name);
-
-        for ($depth = $rii->getDepth() - 1; $depth >= 0; $depth--) {
-            $path = array($rii->getSubIterator($depth)->current()->getFilename() => $path);
-        }
-
-        $tree = array_merge_recursive($tree, $path);
-    }
-
-    return $tree;
+$files = scandir('./directory/to/list');
+sort($files); // this does the sorting
+foreach($files as $file){
+   echo'<a href="/directory/to/list/'.$file.'">'.$file.'</a>';
 }
 
 ?>
