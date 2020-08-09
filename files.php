4985c29b679d6b7a8894bdc271eb2944350c8bac
rdcravens <rodger.cravens@gmail.com>
2020-08-09 10:49:33 -0500

    Create files.php

---
+++ files.php
@@ -0,0 +1,16 @@
+<?php
+
+function getDirContents($path) {
+    $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+
+    $files = array(); 
+    foreach ($rii as $file)
+        if (!$file->isDir())
+            $files[] = $file->getPathname();
+
+    return $files;
+}
+
+var_dump(getDirContents($path));
+
+?>
