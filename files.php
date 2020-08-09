75e31a666d06f9e66fc049b69c14c781e882ac8d
rdcravens <rodger.cravens@gmail.com>
2020-08-09 10:52:03 -0500

    Update files.php

--- files.php
+++ files.php
@@ -1,16 +1,31 @@
 <?php
 
-function getDirContents($path) {
-    $rii = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($path));
+function dir_tree($dir_path)
+{
+    $rdi = new \RecursiveDirectoryIterator($dir_path);
 
-    $files = array(); 
-    foreach ($rii as $file)
-        if (!$file->isDir())
-            $files[] = $file->getPathname();
+    $rii = new \RecursiveIteratorIterator($rdi);
 
-    return $files;
+    $tree = [];
+
+    foreach ($rii as $splFileInfo) {
+        $file_name = $splFileInfo->getFilename();
+
+        // Skip hidden files and directories.
+        if ($file_name[0] === '.') {
+            continue;
+        }
+
+        $path = $splFileInfo->isDir() ? array($file_name => array()) : array($file_name);
+
+        for ($depth = $rii->getDepth() - 1; $depth >= 0; $depth--) {
+            $path = array($rii->getSubIterator($depth)->current()->getFilename() => $path);
+        }
+
+        $tree = array_merge_recursive($tree, $path);
+    }
+
+    return $tree;
 }
-
-var_dump(getDirContents($path));
 
 ?>
